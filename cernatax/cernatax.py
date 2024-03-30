d8a6730d73dc828808982d7d74fa08e60e5656e7
chenlingxi@ <chanlingxi@gmail.com>
2024-03-30 20:00:00 +0800

    add ceRNA_data_lite & expand database & loose DEG

--- cernatax/cernatax.py
+++ cernatax/cernatax.py
@@ -30,6 +30,7 @@ class CERNATAX():
         df = pd.merge(df, deg_df['log2FC'], left_on='miRNA', right_index=True)
         df = pd.merge(df, deg_df['log2FC'], left_on='ceRNA', right_index=True)
         df.columns = ['miRNA', 'ceRNA', 'species', 'database', 'type', 'miRNA_log2FC', 'ceRNA_log2FC']
+        df['inference'] = 'strict'
         ceRNA_df = df[(df.miRNA_log2FC * df.ceRNA_log2FC < 0)]
     
         tmp = ceRNA_df.groupby('miRNA').agg({'type': lambda x: len(set(x))})
@@ -39,3 +40,17 @@ class CERNATAX():
         self.axis_df = axis_df
 
         return ceRNA_df, axis_df        
+    
+    def expand_ceRNA_axis_by_loose_DEG(self, deg_strict_df, deg_df):
+        # just expand ceRNA based on miRNA axis
+        ref_db = self.ref_db
+    
+        axis_miRNA_list = self.axis_df.miRNA.unique()   
+        deg_ceRNA = deg_df[deg_df.type != 'miRNA'].gene.unique()
+        
+        df = ref_db[ref_db.miRNA.isin(axis_miRNA_list) & ref_db.ceRNA.isin(deg_ceRNA)]
+        df = pd.merge(df, deg_strict_df['log2FC'], left_on='miRNA', right_index=True)
+        df = pd.merge(df, deg_df['log2FC'], left_on='ceRNA', right_index=True)
+        df.columns = ['miRNA', 'ceRNA', 'species', 'database', 'type', 'miRNA_log2FC', 'ceRNA_log2FC']
+        df['inference'] = 'loose'
+        return df
